c65104b5331d536fb6b9119625cb7d1c07701888
worotyns <mateusz@pushpushgo.com>
2023-06-13 12:27:31 +0200

    fix exports of types

--- src/server/client/index.ts
+++ src/server/client/index.ts
@@ -4,4 +4,6 @@ export type { ProviderCredential, CredentialType } from "./interfaces/credential
 export type { Bucket } from "./bucket";
 export type { Context } from "./context";
 export type { Receiver } from "./interfaces/receiver";
-export type { RawContextMetadata as ContextMetadata } from "./interfaces/metadata";
+export type { RawContextMetadata } from "./interfaces/metadata";
+export type { DataContextMetadata } from "./interfaces/metadata";
+export type { SilentContextMetadata } from "./interfaces/metadata";
